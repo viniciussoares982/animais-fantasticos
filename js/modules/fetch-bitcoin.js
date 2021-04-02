b9e3507eab693a5dc7b276b446dba3215c8a805e
Vinicius S <vinicius.soares852@gmail.com>
2021-04-02 09:56:05 -0300

    refatorar fetch Bitcoin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,6 +1,6 @@
-export default function initfetchBitcoin() {
+export default function fetchBitcoin(url, target) {
   function bitcoinBRL(btcPrice) {
-    const btcHtmlPrice = document.querySelector('.btc-price')
+    const btcHtmlPrice = document.querySelector(target)
     const btcPriceBRL = btcPrice.sell
 
     const btcReal = (1000 / btcPriceBRL).toFixed(4)
@@ -9,8 +9,9 @@ export default function initfetchBitcoin() {
 
   async function fetchBTC() {
     try {
-      const responseBitcoin = await fetch('https://blockchain.info/ticker')
+      const responseBitcoin = await fetch(url)
       const bitcoinJSON = await responseBitcoin.json()
+
       bitcoinBRL(bitcoinJSON.BRL)
     }catch(err) {
       console.log(Error(err))
